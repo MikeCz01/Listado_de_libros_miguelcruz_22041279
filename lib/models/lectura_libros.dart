86865cfd41f162aaeef586b097981139ea4b089d
Miguel Alejandro Cruz <miguelcruz5734@gmail.com>
2024-02-16 19:58:22 -0600

    Se actualizaron algunas dependencias necesarias para el proyecto

---
+++ lib/models/lectura_libros.dart
@@ -0,0 +1,14 @@
+import 'dart:convert' as convert;
+import 'package:flutter/services.dart';
+import 'package:examen_primero_miguelcruz/models/libros.dart';
+
+//Estructura de la clase
+class _libroslecturaarch{
+  Future<List<Libros>> getlibros() async {
+    final dataAssetBundle = await rootBundle.loadString('database/libros.json');
+    List<dynamic> equiposListJson = convert.jsonDecode(dataAssetBundle);
+    return equiposListJson.map((e) => Libros.fromJson(e)).toList();
+  }
+}
+
+final libroslecturaarch = _libroslecturaarch();
