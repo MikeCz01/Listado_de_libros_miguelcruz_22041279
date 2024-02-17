db2365394540b70e64377d227cbc6cb54b65c43c
Miguel Alejandro Cruz <miguelcruz5734@gmail.com>
2024-02-16 19:49:06 -0600

    Se agregaron las carpetas y se creó la home page

--- lib/screens/lista_libros.dart
+++ lib/screens/lista_libros.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_equipos_futbol/core/utils/equipos_read_file.dart';
 //import 'package:flutter_equipos_futbol/presentation/pages/home_page/widgets/items_equipos.dart';
 
-Widget libreria() {
+/*Widget libreria() {
   return FutureBuilder(
     future: equiposReadFile.getEquipos(), 
     builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
@@ -14,5 +14,4 @@ Widget libreria() {
         return const CircularProgressIndicator();
       }
     }
-  );
-}
+  );*/
